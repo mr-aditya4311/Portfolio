a70e99442da4ce07ded9bb2adb12283307b69aac
Aditya Verma <cuteaditya4311@gmail.com>
2025-10-14 19:33:53 +0530

    Delete appp.js

--- appp.js
+++
@@ -1,7 +0,0 @@
-alert("JavaScript is connected!");
-console.log("JavaScript is running!");
-
-document.addEventListener("DOMContentLoaded", () => {
-  const heading = document.querySelector("h1");
-  heading.textContent = "Welcome to My Portfolio!";
-});
